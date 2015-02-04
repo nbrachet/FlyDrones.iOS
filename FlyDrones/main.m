318b4d3b96ec429ec24af2b55fa36c909a773f51
Sergey Galagan <galagan@qarea.com>
2015-02-04 13:26:30 +0200

    configured app`s bundle identifier

--- FlyDrones/main.m
+++ FlyDrones/main.m
@@ -6,11 +6,12 @@
 //  Copyright (c) 2015 Sergey Galagan. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
-int main(int argc, char * argv[]) {
-    @autoreleasepool {
+int main(int argc, char * argv[])
+{
+    @autoreleasepool
+    {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
